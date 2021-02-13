b9647959c7fab867de0ec3255e8893c0d2f29293
Megumi-0712 <toruansya@yahoo.co.jp>
2021-02-13 21:18:01 +0900

    Add Readme.md & 改行練習

--- helloruby.rb
+++ helloruby.rb
@@ -1,6 +1,15 @@
 print("Hello, Ruby.\n")
 
-#putsとpの違いは、出力した後に改行すが、printは改行しない
+#putsとpの違いは、出力した後に改行するが、printは改行しない
 #\nとは、改行を表す。
 
-print("Hello, \nRuby\n!\n")
+print("Hello, \"Ruby\".\n")
+#文中の\はそれに続く文字に特別な意味を持たせる文字になっている
+
+print('Hello, \nRuby\n!\n')
+#シングルクォートで入力した際、文章そのままを表示される
+
+print("\n")
+
+print('Hello, \\ \'Ruby\'.')
+#\と'を文字そのものとして含めておきたい時限定で文字の前に\を追加する
