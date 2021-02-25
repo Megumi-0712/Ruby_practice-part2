31a4eff6e8b5885ceaad43d1c246082a9dc866aa
Megumi-0712 <toruansya@yahoo.co.jp>
2021-02-25 23:38:27 +0900

    add comment_sample.rb

--- area_volume.rb
+++ area_volume.rb
@@ -5,3 +5,8 @@ area = (x*y + y*z + z*x) * 2
 volume = x * y * z
 print "表面積＝", area, "\n"
 print "体積=", volume, "\n"
+
+#変数には値が何を表しているのかを明確にするという意味もある。つまり、わかりやすい名前を付けたほうがいい
+
+print "表面積＝#{area}\n" 	#printの場合
+puts "表面積＝#{area}"		#putsの場合
---
+++ comment_sample.rb
@@ -0,0 +1,17 @@
+=begin
+「Rubyの復習の時間」
+コメント練習
+2021/02/25 作成
+
+=end
+
+x = 10		#横
+y = 20		#縦
+z = 30		#高さ
+
+#表面積と体積を練習する
+area = (x*y + y*z + z*x) * 2
+volume = x * y * z
+#出力する
+print "表面積＝", area, "\n"
+print "体積＝", volume, "\n"
