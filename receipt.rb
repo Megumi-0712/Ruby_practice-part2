ff806c374ae0d12eb013aa33865b28dcdd5b1de5
Megumi-0712 <toruansya@yahoo.co.jp>
2021-04-04 23:52:56 +0900

    Add #18 to #31

--- receipt.rb
+++ receipt.rb
@@ -1,6 +1,6 @@
 class Receipt
 	def initialize(name)
-		@name = name
+		@name = name							# インスタンス変数の初期化
 		@lines = []
 	end
 
@@ -15,3 +15,17 @@ class Receipt
 		end
 		total
 	end
+
+	def output
+		puts "レシート #{@name}"
+		@lines.each do |line|
+			puts "#{line[:name]} #{line[:price]}円 x #{line[:num]}"
+		end
+		puts "合計金額: #{calc}円"
+	end
+end
+
+r = Receipt.new("ストアA")
+r.lines = [{name: "卵", price: 200, num: 1},
+			{name:"大根", price: 100, num: 2}]
+r.output
