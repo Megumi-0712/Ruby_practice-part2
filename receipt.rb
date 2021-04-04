70d05450f6efc4dfda3baf7c36263208f11ecc74
Megumi-0712 <toruansya@yahoo.co.jp>
2021-04-04 23:36:43 +0900

    Add 7rb and 17rb

--- receipt.rb
+++ receipt.rb
@@ -3,3 +3,15 @@ class Receipt
 		@name = name
 		@lines = []
 	end
+
+	def lines = (lines)
+		@lines = lines
+	end
+
+	def calc
+		total = 0
+		@lines.each do |lime|
+			total += line[:price] * line[:num]
+		end
+		total
+	end
