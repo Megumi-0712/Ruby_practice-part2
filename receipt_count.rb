e002a013342660139db89eeef285e7e75fc60add
Megumi-0712 <toruansya@yahoo.co.jp>
2021-04-13 23:57:56 +0900

    Add #8 to #31

--- receipt_count.rb
+++ receipt_count.rb
@@ -5,4 +5,30 @@ class Receipt
 		@@count
 	end
 
+	def initialize(name)
+		@name = name
+		@lines = []
+	end
+
+	def lines = (lines)
+		@lines = lines
+	end
+
+	def calc
+		total = 0
+		@lines.each do |line|
+			total += line[:price] * line[:num]
+		end
+		total
+	end
+
+	def output
+		puts "レシート #{@name}"
+		@lines.each do |line|
+			puts "#{line[:name]} #{line[:price]}円 x #{line[:num]}"
+		end
+		puts "合計金額: #{calc}円"
+		@@count += 1
+	end
+
 end
