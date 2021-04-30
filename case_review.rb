0c3a7112b53aaca2be4ab3c0c08fc27392a400b0
Megumi-0712 <toruansya@yahoo.co.jp>
2021-05-01 00:10:10 +0900

    Add #7 to #12

--- case_review.rb
+++ case_review.rb
@@ -3,5 +3,10 @@ tags ["A", "B", "C"]
 tags.each do |tagname|
 	case tagname
 	when "G","A", "I", "E"
-		puts "#{tagname} has a child"
-		
+		puts "#{tagname} has a child."
+	when "B", "W"
+		puts "#{tagname} has no child."
+	else
+		puts "#{tagname} can't be used."
+	end
+end
