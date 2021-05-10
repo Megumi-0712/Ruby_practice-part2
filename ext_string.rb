1c5866137083c0c40a1d3db0cb1965d09c15a867
Megumi-0712 <toruansya@yahoo.co.jp>
2021-05-10 23:38:53 +0900

    Add:9行目から10行目追記

--- ext_string.rb
+++ ext_string.rb
@@ -1,7 +1,10 @@
 class String
 	def count_word
-		ary = self.split(" ")	#selfを空白文字区切りで
-								#配列に分解する
-		return ary.size			#分解後の配列の要素数を返す
+		ary = self.split(" ")		#selfを空白文字区切りで
+									#配列に分解する
+		return ary.size				#分解後の配列の要素数を返す
 	end
 end
+
+str = "Just Another Ruby Newbie"	#4
+p str.count_word
