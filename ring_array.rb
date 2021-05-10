5c657cdc5c981372824701843d0b8772c9f5924a
Megumi-0712 <toruansya@yahoo.co.jp>
2021-05-10 23:50:08 +0900

    Add:15～20行目でプログラムを実践した

--- ring_array.rb
+++ ring_array.rb
@@ -4,3 +4,17 @@ class RingArray < Array 		#スーパークラスの指定
 		super(idx)				#スーパークラスの同名メソッドを呼ぶ
 	end
 end
+
+wday = RingArray["日","月","火","水","木","金","土"]
+
+p wday[6]
+p wday[11]
+p wday[15]
+p wday[-1]
+
+try = RingArray["火","水","風","林","光","闇","無"]
+
+p try[2]
+p try[100]
+p try[-20]
+p try[-90]
